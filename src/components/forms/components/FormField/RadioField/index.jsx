80a2e42f5e92bc5b6e27f96ede2c8015d62a28fe
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-09-15 23:16:06 +0300

    Add RadioField styles

--- src/components/forms/components/FormField/RadioField/index.jsx
+++ src/components/forms/components/FormField/RadioField/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useField } from 'formik';
 import PropTypes from 'prop-types';
+import styles from './radio.module.scss';
 
 function RadioField(props) {
   const [field] = useField(props);
   const { label, description } = props;
 
   return (
-    <div>
-      <input {...field} {...props} />
-      <div>
-        <h2>{label}</h2>
-        <h3>{description}</h3>
+    <div className={styles.container}>
+      <input {...field} {...props} className={styles.checkInput} />
+      <div className={styles.infoContainer}>
+        <h2 className={styles.header}>{label}</h2>
+        <h3 className={styles.subInfo}>{description}</h3>
       </div>
     </div>
   );
